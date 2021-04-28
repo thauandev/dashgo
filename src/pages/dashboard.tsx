8e3e5f6150ff06d861d9dbc4931033ecee31b5fd
thauandev <thauandepaula@icloud.com>
2021-04-28 11:10:47 -0300

    Dashboard responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -67,14 +67,14 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="large" mb="4">
               Inscritos da Semana
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius={8}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8}>
             <Text fontSize="large" mb="4">
               Taxa de abertura
             </Text>
